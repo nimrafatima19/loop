309b3f4f1941a73569c788a9fcbaa0306e08aa1b
nimrafatima19 <145598666+nimrafatima19@users.noreply.github.com>
2023-12-19 12:09:17 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// For loop
+// for(i=0;i<=5;i++) {
+//     document.write("Nimra" + "<br/>")
+// }
+// ab = +prompt("Enter any number");
+// for(i=1;i<=10;i++) {
+// document.write(ab + "x" + i + "=" + ab*i + "<br/>");
+// }
+nim  = prompt("Enter any number");
+for(i=1;i<=10;i++) {
+    document.write(nim + "x" + i + "=" + nim*i + "<br/>");
+}
